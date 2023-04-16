c393d10269d5a365d4e67712e3ddb9dc8fbc2a99
phatlt <phatlt@jienielab.asia>
2023-04-16 11:29:38 +0700

    update center class list

--- src/actions/master/class-action.js
+++ src/actions/master/class-action.js
@@ -81,6 +81,87 @@ getClassList = async function (req, res) {
   }
 };
 
+getCenterClassList = async function (req, res) {
+  try {
+    const itemsPerPage = req.body.pagination.itemsPerPage || 10;
+    const currentPage = req.body.pagination.currentPage || 1;
+    const statusList =
+      req.body.query.statusSelecteds && req.body.query.statusSelecteds.length
+        ? req.body.query.statusSelecteds
+        : [];
+    const keywordSearch = req.body.query.keywordSearch;
+
+    var searchSQL = "";
+    searchSQL += statusList.length
+      ? "center_classes.status = ANY($1) "
+      : "(center_classes.status = ANY($1) OR TRUE = TRUE) ";
+    searchSQL += keywordSearch
+      ? `AND (LOWER(center_classes.class_code) LIKE '%' || LOWER($2) || '%'
+            OR LOWER(static_address.name) LIKE '%' || LOWER($2) || '%'
+            OR LOWER(static_grade.name) LIKE '%' || LOWER($2) || '%') `
+      : "AND (center_classes.class_code = $2 OR TRUE = TRUE) ";
+
+    const selectSql = `SELECT
+      center_classes.id AS "id",
+      center_classes.class_code AS "classCode",
+      center_classes.register_name AS "registerName",
+      static_address.name AS "addressProvince",
+      center_classes.address_detail AS "addressDetail",
+      center_classes.register_phone AS "registerPhone",
+      static_grade.name AS "grade",
+      static_subject.name AS "subject",
+      center_classes.sessions_per_week AS "sessionsPerWeek",
+      center_classes.opening_day AS "openingDay",
+      center_classes.note AS "note",
+      center_classes.status AS "status",
+      center_classes.registration_date AS "registrationDate",
+      center_classes.tutor_type AS "tutorType",
+      center_classes.tuition AS "tuition"`;
+    const countSql = `SELECT COUNT(center_classes.id) as count `;
+    const conditionSql = `FROM center_classes
+      LEFT JOIN static_address
+        ON static_address.id = center_classes.address_id
+      LEFT JOIN static_grade
+        ON static_grade.id = center_classes.grade_id
+      LEFT JOIN static_subject
+        ON static_subject.id = center_classes.subject_id
+      WHERE
+        ${searchSQL} `;
+    const pagingAndSortSql = 
+      `ORDER BY center_classes.registration_date DESC, center_classes.id DESC
+      LIMIT $3 OFFSET $4`;
+
+    const sqlSelectInputValues = [
+      statusList,
+      keywordSearch,
+      itemsPerPage,
+      (currentPage - 1) * itemsPerPage,
+    ];
+    const sqlCountInputValues = [statusList, keywordSearch];
+
+    const sqlClassList = await pool.query(
+      selectSql + conditionSql + pagingAndSortSql,
+      sqlSelectInputValues
+    );
+    const sqlCount = await pool.query(
+      countSql + conditionSql,
+      sqlCountInputValues
+    );
+    const count = sqlCount.rows[0].count;
+
+    res.status(200).send({
+      classList: sqlClassList.rows,
+      itemsPerPage: itemsPerPage,
+      currentPage: currentPage,
+      totalClasses: count,
+    });
+  } catch (err) {
+    console.error("load class list failed:", err);
+    res.status(400).send({ mes: err });
+  }
+};
+
 module.exports = {
   getClassList,
+  getCenterClassList
 };
